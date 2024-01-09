04b0d46f416fa7f01ee4a2d0a1df9c44d61c1e8e
jun <jun@home>
2024-01-08 20:09:49 -0800

    whoops update vite config

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         lSystem: resolve(__dirname, 'lSystem/index.html'),
+        veraMolnar: resolve(__dirname, 'veraMolnar/index.html'),
       },
     },
   },
