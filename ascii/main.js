98003bd2c40b7a1e6a45b8431d2821beb708884a
jun <jun@home>
2024-01-11 21:33:44 -0800

    add colors

--- ascii/main.js
+++ ascii/main.js
@@ -6,14 +6,14 @@ const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d', {willReadFrequently: true});
 
 const canvasGray = document.createElement('canvas');
-const ctxGray = canvasGray.getContext('2d');
+const ctxGray = canvasGray.getContext('2d', {willReadFrequently: true});
 const img = new Image();
 
-const canvasText = document.createElement('canvas')
-const ctxText = canvasText.getContext('2d', {willReadFrequently: true})
+const canvasText = document.createElement('canvas');
+const ctxText = canvasText.getContext('2d', {willReadFrequently: true});
 
 const asciiDiv = document.createElement('div');
-main.appendChild(asciiDiv)
+main.appendChild(asciiDiv);
 
 const ASCIIS = [
   '.',
@@ -36,7 +36,6 @@ ASCIIS.reverse();
 
 img.crossOrigin = 'anonymous';
 img.src = imgSrc;
-
 
 img.addEventListener('load', () => {
   canvas.width = 500;
@@ -65,7 +64,7 @@ function createGrayscale() {
   content.appendChild(canvasGray);
 }
 
-function getGrayscalePixel(data, print=false) {
+function getGrayscalePixel(data, print = false) {
   let r = 0,
     g = 0,
     b = 0;
@@ -79,35 +78,35 @@ function getGrayscalePixel(data, print=false) {
     }
   });
   if (print) {
-    console.log(r, g, b)
+    console.log(r, g, b);
   }
   const tot = Math.floor(data.length / 4);
-  return 0.2126 * (r/tot) + 0.7152 * (g/tot) + 0.0722 * (b/tot);
+  return 0.2126 * (r / tot) + 0.7152 * (g / tot) + 0.0722 * (b / tot);
 }
 
 function createASCII() {
   canvasText.width = 500;
   canvasText.height = 500;
   const SIZE = 12;
+  const rnd = Math.floor(Math.random() * 255)
   ctxText.font = `${SIZE}px monospace`;
-  for (let i = 0; i < canvasText.width; i+= SIZE) {
+  for (let i = 0; i < canvasText.width; i += SIZE) {
     for (let j = 0; j < canvasText.height; j += SIZE) {
+      ctxText.fillStyle = `rgb(${Math.floor(i - 32.5 * (500 / SIZE / 255))}, ${Math.floor(j * (500 / SIZE / 255))}, ${rnd})`;
       const {data} = ctxGray.getImageData(i, j, SIZE, SIZE);
-      const gray = getGrayscalePixel(data, true)
-      const chr = getASCII(gray)
+      const gray = getGrayscalePixel(data);
+      const chr = getASCII(gray);
       ctxText.fillText(chr, i, j);
-      console.log(gray)
     }
   }
-  content.appendChild(canvasText)
+  content.appendChild(canvasText);
 }
 
 const ASCII_INCREMENT = 255 / (ASCIIS.length + 1);
 function getASCII(val) {
-  const index = Math.floor(val / ASCII_INCREMENT)
-  return ASCIIS[index] || ''
+  const index = Math.floor(val / ASCII_INCREMENT);
+  return ASCIIS[index] || '';
 }
 
-
-asciiDiv.className = 'ascii'
-asciiDiv.innerHTML += ASCIIS.join('')
+asciiDiv.className = 'ascii';
+asciiDiv.innerHTML += ASCIIS.join('');
